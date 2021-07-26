a69a430d30405c1702877e25c5a53f089140be7b
Gerardo Moreno <wanderup.allego@gmail.com>
2021-07-25 20:24:56 -0400

    2.6: The Phonebook Step1

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,26 +1,47 @@
 import React, { useState } from 'react'
+import Contact from './components/Contact'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
+  const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addContact = (event) => {
+    event.preventDefault()
+    const noteObject = { name: newName }
+    setPersons(persons.concat(noteObject))
+    setNewName('')
+  }
+
+  const handlePersonsChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
-    
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={addContact}>
+          <div>
+            name: <input
+              value={newName}
+              onChange={handlePersonsChange}
+            />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
       <h2>Numbers</h2>
-      ...
-      <div>debug: {newName}</div>
+      <div>
+        <ul>
+          {persons.map(person => <Contact key={person.name} contact={person} />)}
+        </ul>
+      </div>
+      {/* <div>debug: {newName}</div> */}
     </div>
   )
 }
---
+++ phonebook/src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = ({ contact }) => {
+  return (
+    <ul>{contact.name}</ul>
+  )
+}
+
+export default Contact
