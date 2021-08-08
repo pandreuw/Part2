1234293effe771d3de1b4e35cea3f61a4a2a60c7
Gerardo Moreno <wanderup.allego@gmail.com>
2021-08-08 09:13:16 -0400

    2.9: The Phonebook Step4, implement search filter

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -5,12 +5,24 @@ import Contact from './components/Contact'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '416-123-4567'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+    { name: 'Gerardo Moreno', number: '99999999' },
+    { name: 'Leslie Vega', number: '3333333333' },
+    { name: 'Saria Regina Moreno Vega', number: '88888888' },
+    { name: 'Elissa Maria Moreno Vega', number: '666666666666' }
   ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const message =`The name "${newName}" was already added to the phonebook
+  const [newFilter, setFilter] = useState('')
+  const [personFilter, setpersonFilter] = useState(persons)
+
+  const message = `The name "${newName}" was already added to the phonebook
 The field will be erased.`
+
   const addContact = (event) => {
     event.preventDefault()
     console.log("Add contact names");
@@ -26,8 +38,11 @@ The field will be erased.`
       setPersons(persons.concat(noteObject))
       setNewName('')
       setNewNumber('')
+      // Following required to reset the filter and update the list with the new entry
+      //unable to add a reduce
+      setFilter('')
+      setpersonFilter(persons.concat(noteObject))
     }
-
   }
 
   const handlePersonsChange = (event) => {
@@ -35,14 +50,41 @@ The field will be erased.`
     setNewName(event.target.value)
   }
 
-  const hnadelNumberChange = (event) => {
+  const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    //console.log(event.target.value)
+    setFilter(event.target.value)
+    applyFilter(event.target.value)
+  }
+
+  const applyFilter = (prop) => {
+    // const OnlyNames = persons.map(onlyname => onlyname.name)
+    // const secondArray = OnlyNames.filter(onlyname => onlyname.includes(prop))
+    // console.log("Apply filter", prop);
+    // console.log('filtered array', secondArray);
+    // from https://stackoverflow.com/questions/2722159/how-to-filter-object-array-based-on-attributes
+    var noteObject = persons.filter(filtering => filtering.name.includes(prop));
+    // console.log('filtered object',noteObject);
+    setpersonFilter(noteObject)
+    console.log('filtered object', noteObject);
+  }
+
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter show with <input
+          value={newFilter}
+          onChange={handleFilterChange}
+        />
+      </div>
+      <h2>Add a new</h2>
       <div>
         <form onSubmit={addContact}>
           <div>
@@ -54,7 +96,7 @@ The field will be erased.`
           <div>
             number: <input
               value={newNumber}
-              onChange={hnadelNumberChange}
+              onChange={handleNumberChange}
             />
           </div>
           <div>
@@ -65,10 +107,11 @@ The field will be erased.`
       <h2>Numbers</h2>
       <div>
         <ul>
-          {persons.map(person => <Contact key={person.name} contact={person} />)}
+          {/* {persons.map(person => { return <Contact key={person.name} contact={person} /> })} */}
+          {personFilter.map(person => { return <Contact key={person.name} contact={person} /> })}
         </ul>
       </div>
-      {/* <div>debug: {newName}</div> */}
+      {/* <div>debug: {personFilter.length}</div> */}
     </div>
   )
 }
