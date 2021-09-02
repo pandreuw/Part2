265d03b6d872cac3b603e047f9d0c849c8fdd75f
Gerardo Moreno <wanderup.allego@gmail.com>
2021-09-01 23:02:51 -0400

    2.12 Data for countries, step1

--- countries/src/components/DisplayCountries.js
+++ countries/src/components/DisplayCountries.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
-const PrintCountryName = ({ _country }) => {
+const PrintCountryList = ({ _country }) => {
     return (
         <ul>{_country.name}</ul>
+    )
+}
+
+const PrintCountryData = ({ _country }) => {
+    return (
+        <>
+            <h2>{_country.name}</h2>
+            <ul>Capital: {_country.capital}</ul>
+            <ul>Population: {_country.population}</ul>
+            <h3>Languages</h3>
+            <ul>
+                {_country.languages.map(lang => {return <PrintLanguages key={lang.name} languages={lang}/>})}
+            </ul>
+            <img src={_country.flag} />
+        </>
+    )
+}
+
+const PrintLanguages = ({languages}) => {
+    return(
+        <li>{languages.name}</li>
     )
 }
 
@@ -16,12 +37,21 @@ const DisplayCountries = ({ CountriesList }) => {
             </div>
         )
     }
+    else if (CountriesList.length === 1) {
+        return (
+            <div>
+                <ul>
+                {CountriesList.map(country => { return <PrintCountryData key={country.name} _country={country} />})}
+                </ul>
+            </div>
+        )
+    }
     else {
         return (
             <div>
                 <ul>
                     {/* {persons.map(person => { return <Contact key={person.name} contact={person} /> })} */}
-                    {CountriesList.map(country => { return <PrintCountryName key={country.name} _country={country} /> })}
+                    {CountriesList.map(country => { return <PrintCountryList key={country.name} _country={country} /> })}
                 </ul>
             </div>
         )
